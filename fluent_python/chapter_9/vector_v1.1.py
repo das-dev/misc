48522e59eb078d0cd6b8aad0d4eacfb49665c43c
das <das.dev@pm.me>
2020-06-20 21:31:42 +0700

    Add an vector object the ability to be formatted

--- fluent_python/chapter_9/vector_v1.1.py
+++ fluent_python/chapter_9/vector_v1.1.py
@@ -1,4 +1,4 @@
-from math import hypot
+from math import hypot, atan2
 from array import array
 
 
@@ -8,10 +8,10 @@ class Vector:
     >>> v.x, v.y
     (3.0, 4.0)
     >>> v
-    Vector2D(3, 4)
+    Vector(3, 4)
     >>> v_clone = eval(repr(v))
     >>> v_clone
-    Vector2D(3, 4)
+    Vector(3, 4)
     >>> v == v_clone
     True
     >>> print(v)
@@ -20,11 +20,24 @@ class Vector:
     >>> octets
     b'd\\x00\\x00\\x00\\x00\\x00\\x00\\x08@\\x00\\x00\\x00\\x00\\x00\\x00\\x10@'
     >>> Vector.frombytes(octets)
-    Vector2D(3, 4)
+    Vector(3, 4)
     >>> abs(v)
     5.0
     >>> bool(v), bool(Vector(0, 0))
     (True, False)
+    >>> format(v)
+    '(3.0, 4.0)'
+    >>> format(v, '.2f')
+    '(3.00, 4.00)'
+    >>> format(v, '.3e')
+    '(3.000e+00, 4.000e+00)'
+    >>> format(Vector(1, 1), 'p')
+    '<1.4142135623730951, 0.7853981633974483>'
+    >>> format(Vector(1, 1), '.3ep')
+    '<1.414e+00, 7.854e-01>'
+    >>> format(Vector(1, 1), '0.5fp')
+    '<1.41421, 0.78540>'
+
     """
 
     typecode = 'd'
@@ -53,6 +66,20 @@ class Vector:
 
     def __bytes__(self):
         return (bytes([ord(self.typecode)])) + (bytes(array(self.typecode, self)))
+
+    def __format__(self, format_spec):
+        if format_spec.endswith('p'):
+            format_spec = format_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_format = '<{}, {}>'
+        else:
+            coords = self
+            outer_format = '({}, {})'
+        components = (format(c, format_spec) for c in coords)
+        return outer_format.format(*components)
+
+    def angle(self):
+        return atan2(self.y, self.x)
 
     @classmethod
     def frombytes(cls, octets):
