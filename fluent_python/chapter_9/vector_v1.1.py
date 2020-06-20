c430a4f10c1f1045037d90698621069b3072ce14
das <das.dev@pm.me>
2020-06-20 22:10:13 +0700

    Add casting to int, float and complex for vector

--- fluent_python/chapter_9/vector_v1.1.py
+++ fluent_python/chapter_9/vector_v1.1.py
@@ -8,10 +8,10 @@ class Vector:
     >>> v.x, v.y
     (3.0, 4.0)
     >>> v
-    Vector(3, 4)
+    Vector(3.0, 4.0)
     >>> v_clone = eval(repr(v))
     >>> v_clone
-    Vector(3, 4)
+    Vector(3.0, 4.0)
     >>> v == v_clone
     True
     >>> print(v)
@@ -20,7 +20,7 @@ class Vector:
     >>> octets
     b'd\\x00\\x00\\x00\\x00\\x00\\x00\\x08@\\x00\\x00\\x00\\x00\\x00\\x00\\x10@'
     >>> Vector.frombytes(octets)
-    Vector(3, 4)
+    Vector(3.0, 4.0)
     >>> abs(v)
     5.0
     >>> bool(v), bool(Vector(0, 0))
@@ -37,17 +37,33 @@ class Vector:
     '<1.414e+00, 7.854e-01>'
     >>> format(Vector(1, 1), '0.5fp')
     '<1.41421, 0.78540>'
-
+    >>> v.x = 7
+    Traceback (most recent call last):
+    ...
+    AttributeError: can't set attribute
+    >>> v2, v3 = Vector(3.0, 4.0), Vector(3.1, 4.1),
+    >>> hash(v), hash(v2), hash(v3)
+    (7, 7, 1031)
+    >>> v == v2
+    True
+    >>> {v, v2, v3}
+    {Vector(3.1, 4.1), Vector(3.0, 4.0)}
+    >>> int(v3)
+    5
+    >>> float(v3)
+    5.140038910358559
+    >>> complex(v3)
+    (3.1+4.1j)
     """
 
     typecode = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
 
     def __repr__(self):
-        return f'{type(self).__name__}({self.x:.0f}, {self.y:.0f})'
+        return f'{type(self).__name__}({self.x!r}, {self.y!r})'
 
     def __str__(self):
         return str(tuple(self))
@@ -77,6 +93,26 @@ class Vector:
             outer_format = '({}, {})'
         components = (format(c, format_spec) for c in coords)
         return outer_format.format(*components)
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
+    def __int__(self):
+        return int(abs(self))
+
+    def __float__(self):
+        return abs(self)
+
+    def __complex__(self):
+        return complex(self.x, self.y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def angle(self):
         return atan2(self.y, self.x)
